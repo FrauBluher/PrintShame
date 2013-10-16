ef7a5a74f9271a43b8f535d1631019158218dcbc
Pavlo Manovi <pavlo@Snorlax.(none)>
2013-10-16 13:00:49 -0700

    Now returns top ten.

--- printshame.py
+++ printshame.py
@@ -2,7 +2,7 @@
 #Gets and updates new stuff
 
 import urllib.parse, urllib.request, datetime
-import http.cookiejar, urllib.request, string
+import http.cookiejar, urllib.request, string, re
 
 #Get current time information.
 Now = datetime.datetime.now()
@@ -34,27 +34,48 @@ else:
 
 Yesterday = str(month) + '/' + str(day) + '/' + str(year)
 
+#Printer object for later use with priting data
+class Printer(object):
+    ident = ""
+    pages = ""
+    percentage = ""
+
+    def __init__(self, ident, pages, percentage):
+        self.ident = ident
+        self.pages = pages
+        self.percentage = percentage
+
+def make_printer(ident, pages, percentage):
+    printer = Printer(ident, pages, percentage)
+    return printer
+
 #POST and GET http header construction and tx/rx.
+#Here we make the first handshake to get our cookies.
 cj = http.cookiejar.CookieJar()
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
 r = opener.open("https://support.soe.ucsc.edu/printing-reports/user")
-
-#Debugging output
-#for cookie in cj:
-#	print(cookie)
-#	cookieheader = {'cookie':cookie.name + "=" + cookie.value}
-#print(cookieheader)
 
 #Looking for the following string name="form_build_id"
 firstform = r.read().decode('utf-8')
 index = firstform.index('form_build_id')
 id = firstform[index+19:index+56]
-#print(id)
 
+#Header data construction.
 data = urllib.parse.urlencode({'start_date': Yesterday, 'end_date': Today, 'op': 'Update+Report', 'form_build_id': id, 'form_id': 'soe_printing_user'})
 data = data.encode('utf-8')
 
+#Get user reports from Today to Yesterday.
 f = opener.open("https://support.soe.ucsc.edu/printing-reports/user", data)
-print(f.read().decode('utf-8'))
+
+#Decode, find the user info, and extract it.
+secondform = f.read().decode('utf-8')
+index2 = firstform.index('odd')
+topten = secondform[index2:index2+2700]
+m = re.findall(r'((?<=\">|g>)[A-z0-9():\s\-\.\,]*(?=\</.))', topten)
+
+print(m)
+#for i in range(len(m)-3):
+#	make_printer(m[i], m[i+1], m[i+2])
+
 
 #Todo, scrape session ID from initial get, and then send that and the cookie in a new header to get the timespan that I need.
